3b27e01a8ce4e302db2009f9b0c7bd52f1eb4f34
Gábor Csárdi <csardi.gabor@gmail.com>
2020-08-30 12:24:26 +0100

    Printing coverage table

--- R/coverage.R
+++ R/coverage.R
@@ -36,15 +36,21 @@ test <- function(filter = NULL, ...) {
     }
   )
 
-  cv <- as.list(asNamespace("covr")$.counters)
-  cv <- cv[vapply(cv, function(x) length(x$value) != 0, logical(1))]
-  class(cv) <- "coverage"
+  rcv <- as.list(asNamespace("covr")$.counters)
+  rcv <- rcv[vapply(rcv, function(x) length(x$value) != 0, logical(1))]
+  class(rcv) <- "coverage"
+
+  coverage <- create_coverage_table(rcv)
+  cat("\n")
+  print(coverage)
 
   invisible(structure(
-    list(tests = results, coverage = cv),
+    list(tests = results, coverage = coverage, raw_coverage = rcv),
     class = "testthat_coverage"
   ))
 }
+
+# ------------------------------------------------------------------------
 
 interactive_reporter <- R6::R6Class("interactive_reporter",
   inherit = testthat::ProgressReporter,
@@ -170,6 +176,14 @@ issue_summary <- function(x) {
 }
 
 styles <- new.env(parent = emptyenv())
+
+style_orange <- function(...) {
+  if (is.null(styles$orange)) {
+    styles$orange <- cli::make_ansi_style("orange")
+  }
+  styles$orange(...)
+}
+
 style_bg_blue <- function(...) {
   if (is.null(styles$bg_blue)) {
     styles$bg_blue <- cli::make_ansi_style("#0000ff", bg = TRUE)
@@ -189,7 +203,13 @@ style_bg_green <- function(...) {
     styles$bg_green <- cli::make_ansi_style("darkgreen", bg = TRUE)
   }
   styles$bg_green(...)
+}
 
+style_bg_grey <- function(...) {
+  if (is.null(styles$bg_grey)) {
+    styles$bg_grey <- cli::make_ansi_style("#404040", bg = TRUE)
+  }
+  styles$bg_grey(...)
 }
 
 format_type <- function(type) {
@@ -237,6 +257,143 @@ spinner <- function(frames, i) {
   frames[((i - 1) %% length(frames)) + 1]
 }
 
-# We need to import something from covr, otherwise the
-# check freaks out
-fix_check <- function() covr::package_coverage
+# ------------------------------------------------------------------------
+
+create_coverage_table <- function(rcv) {
+  byline <- covr::tally_coverage(rcv, by = "line")
+  byexpr <- covr::tally_coverage(rcv, by = "expression")
+
+  pctfun <- function(x) (sum(x > 0) / length(x)) * 100
+  pctline <- tapply(byline$value, byline$filename, pctfun)
+  pctexpr <- tapply(byexpr$value, byexpr$filename, pctfun)
+
+  pkgline <- pctfun(byline$value)
+  pkgexpr <- pctfun(byexpr$value)
+
+  key <- sort(names(pctline))
+
+  uncov <- lapply(key, function(fn) {
+    sel <- byline[byline$filename == fn, , drop = FALSE]
+    sel <- sel[order(sel$line), , drop = FALSE]
+    find_zero_ranges(sel$line, sel$value)
+  })
+
+  tab <- data.frame(
+    stringsAsFactors = FALSE,
+    file = c("R files", key),
+    pct_lines = c(pkgline, pctline[key]),
+    pct_exprs = c(pkgexpr, pctexpr[key]),
+    uncovered = I(c(list(NULL), uncov))
+  )
+
+  class(tab) <- c("coverage_table", class(tab))
+  tab
+}
+
+# key is ordered
+
+find_zero_ranges <- function(key, value) {
+  out <- list()
+  start <- end <- NA_integer_
+  for (i in seq_along(key)) {
+    if (value[i] == 0 && is.na(start)) {
+      start <- end <- key[i]
+    } else if (value[i] == 0 && !is.na(start)) {
+      end <- key[i]
+    } else if (value[i] != 0 && !is.na(start)) {
+      out[[length(out) + 1]] <- start:end
+      start <- end <- NA_integer_
+    } else if (value[i] != 0 && is.na(start)) {
+      # Nothing to do
+    }
+  }
+
+  if (!is.na(start)) out[[length(out) + 1]] <- start:end
+
+  out
+}
+
+#' @export
+
+print.coverage_table <- function(x, ...) {
+  cat(format(x, ...), sep = "\n")
+}
+
+cov_col <- function(txt, val) {
+  ifelse(
+    val < 75,
+    cli::col_red(txt),
+  ifelse(val < 95, style_orange(txt), txt)
+  )
+}
+
+#' @export
+
+format.coverage_table <- function(x, ...) {
+  fn <- paste0("R/", basename(x$file))
+  fn[-1] <- paste0(" ", fn[-1])
+  bl <- format_pct(x$pct_lines)
+  be <- format_pct(x$pct_exprs)
+
+  cffn <- ffn <- format(c("code coverage", "", fn, "", "total"))
+  cfbl <- fbl <- format(c("% lines", "", bl, "", bl[1]), justify = "right")
+  cfbe <- fbe <- format(c("% exprs", "", be, "", be[1]), justify = "right")
+
+  mid <- 3:(length(ffn) - 2)
+  cffn[mid] <- cov_col(ffn[mid], pmin(x$pct_lines, x$pct_exprs))
+  cfbl[mid] <- cov_col(fbl[mid], x$pct_lines)
+  cfbe[mid] <- cov_col(fbe[mid], x$pct_exprs)
+
+  lines <- paste0(
+    cffn, " │ ",
+    cfbl, " │ ",
+    cfbe, " │ "
+  )
+
+  maxw <- max(crayon::col_nchar(lines, type = "width"))
+  cw <- cli::console_width()
+
+  uc <- vapply(x$uncovered, format_uncovered, "", width = cw - maxw)
+  cuc <- format(c("uncovered line #", "", uc, "", ""))
+  cuc[mid] <- cov_col(cuc[mid], pmin(x$pct_lines, x$pct_exprs))
+  lines <- paste0(lines, cuc)
+
+  lines[1] <- cli::style_bold(style_bg_grey(cli::col_white(lines[1])))
+  tot <- min(x$pct_lines[1], x$pct_exprs[1])
+  if (tot < 75) {
+    lines[length(lines)] <- cli::bg_red(lines[length(lines)])
+  } else if (tot < 95) {
+    lines[length(lines)] <-
+      style_bg_orange(cli::col_white(lines[length(lines)]))
+  } else {
+    lines[length(lines)] <-
+      style_bg_greem(cli::col_white(lines[length(lines)]))
+  }
+  lines[length(lines)] <- cli::style_bold(lines[length(lines)])
+
+  lines
+}
+
+# TODO: consider width
+
+format_uncovered <- function(ranges, width = 80) {
+  rstr <- vapply(ranges, FUN.VALUE = character(1), function(r) {
+    if (length(r) == 1) as.character(r) else paste0(r[1], "-", r[length(r)])
+  })
+
+  rstr[- length(rstr)] <- paste0(rstr[- length(rstr)], ", ")
+
+  if (length(rstr) >= 3) {
+    cumw <- cumsum(nchar(rstr))
+    if (cumw[length(cumw)] > width) {
+      last <- rev(which(cumw <= width - 3))[1]
+      rstr <- c(rstr[1:last], "...")
+    }
+  }
+
+  paste(rstr, collapse = "")
+}
+
+format_pct <- function(x) {
+  paste0(format(x, width = 3, digits = 3, justify = "right"), "%")
+}
