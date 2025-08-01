7c6eaa26427e4d40e28733bdeffa4e1cc4dce860
Gábor Csárdi <csardi.gabor@gmail.com>
2025-08-01 09:48:47 +0200

    Move fixture tests to proper place

---
+++ tests/testthat/fixtures/test.R
@@ -0,0 +1,21 @@
+
+context("testthatlabs")
+
+test_that("testthatlabs works", {
+  for (i in 1:10) expect_true(TRUE)
+})
+
+test_that("lorem ipsum", {
+  for (i in 1:10) expect_true(TRUE)
+  expect_true(FALSE)
+  for (i in 1:5) expect_true(TRUE)
+})
+
+test_that("voluptate adipisicing", {
+  for (i in 1:10) expect_true(TRUE)
+  skip("not this time")
+})
+
+test_that("eiusmod mollit ad id", {
+  expect_true(TRUE)
+})
