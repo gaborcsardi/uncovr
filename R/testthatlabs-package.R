bd7d0fec36acfc32c873e9b046c3e2a8e4b06e6b
Gábor Csárdi <csardi.gabor@gmail.com>
2025-08-01 09:54:41 +0200

    Work around not-imported-from warning

--- R/testthatlabs-package.R
+++ R/testthatlabs-package.R
@@ -4,3 +4,9 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+# to work around not-imported-from R CMD check warning
+dummy <- function() {
+  R6::R6Class
+  prettyunits::pretty_dt
+}
