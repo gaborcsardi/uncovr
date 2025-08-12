1c70bda3cec518e8a364d68dfe1dc5891a8d5162
Gábor Csárdi <csardi.gabor@gmail.com>
2025-08-12 15:46:19 +0200

    Support lcov exclusion patterns as well

--- R/load-dev.R
+++ R/load-dev.R
@@ -501,16 +501,23 @@ parse_line_exclusions <- function(lns, path) {
 
 parse_line_exclusions_single <- function(lns, path) {
   which(
-    grepl("__NO_COVERAGE__$", lns) |
-      grepl("#[ ]*nocov$", lns) |
-      grepl("//[ ]*nocov$", lns) |
-      grepl("/[*][ ]*nocov[ ]* [*]/$", lns)
+    grepl("__NO_COVERAGE__", lns) |
+      grepl("#[ ]*nocov", lns) |
+      grepl("//[ ]*nocov", lns) |
+      grepl("/[*][ ]*nocov[ ]* [*]/", lns) |
+      grepl("LCOV_EXCL_LINE", lns)
   )
 }
 
 parse_line_exclusions_ranges <- function(lns, path) {
-  start <- grep("#[ ]*nocov[ ]+(start|begin)", lns)
-  end <- grep("#[ ]*nocov[ ]+end", lns)
+  start <- unique(sort(c(
+    grep("#[ ]*nocov[ ]+(start|begin)", lns),
+    grep("LCOV_EXCL_START", lns)
+  )))
+  end <- unique(sort(c(
+    grep("#[ ]*nocov[ ]+end", lns),
+    grep("LCOV_EXCL_STOP", lns)
+  )))
 
   length(start) <- length(end) <- max(length(start), length(end))
   if (length(start) == 0) {
