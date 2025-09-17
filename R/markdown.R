358ee4cbca78d3a86a6d0c06c4d428260b8eb432
Gábor Csárdi <csardi.gabor@gmail.com>
2025-09-18 01:55:08 +0200

    Improve markdown report.

--- R/markdown.R
+++ R/markdown.R
@@ -27,16 +27,35 @@ markdown <- function(path = ".", coverage = NULL, output = NULL) {
   # correct order
   coveragex <- coveragex[c(1, path_order(coveragex$path[-1]) + 1), ]
 
-  # indent according to directory structure
-  coveragex$path <- paste0(
-    strrep("&nbsp;", str_count(sub("/$", "", coveragex$path), "/")),
+  # Note original path w/o formatting
+  paths <- coveragex$path
+
+  # summaries bold
+  is_sum_row <- paths == "All files" | endsWith(paths, "/")
+  coveragex$path <- ifelse(
+    is_sum_row,
+    paste0("**", coveragex$path, "**"),
     coveragex$path
   )
 
-  # summaries bold
+  # Links for file names
+  baseurl <- paste0(
+    Sys.getenv("GITHUB_SERVER_URL"),
+    "/",
+    Sys.getenv("GITHUB_REPOSITORY"),
+    "/blob/",
+    Sys.getenv("GITHUB_SHA"),
+    "/"
+  )
   coveragex$path <- ifelse(
-    coveragex$path == "All files" | endsWith(coveragex$path, "/"),
-    paste0("**", coveragex$path, "**"),
+    is_sum_row,
+    coveragex$path,
+    paste0("[`", coveragex$path, "`](", baseurl, "/", paths, ")")
+  )
+
+  # indent according to directory structure
+  coveragex$path <- paste0(
+    strrep("&nbsp;", str_count(sub("/$", "", paths), "/")),
     coveragex$path
   )
 
@@ -68,7 +87,42 @@ markdown <- function(path = ".", coverage = NULL, output = NULL) {
   total_funcs_percent <- format_percent(total_funcs)
   total_funcs_emoji <- format_emoji(total_funcs)
 
-  vars = list(
+  tests <- attr(coverage, "test_results")
+  test_data <- markdown_test_summary(tests)
+  byfile <- testthat_results_by_file(test_data)
+
+  totals <- list(
+    ok = sum(byfile$success),
+    fail = sum(byfile$broken),
+    warn = sum(byfile$warning),
+    skip = sum(byfile$skip)
+  )
+
+  test_files <- with(
+    byfile,
+    paste0(
+      "|[",
+      file,
+      "](",
+      baseurl,
+      "/tests/testthat/",
+      file,
+      ")|",
+      broken,
+      "|",
+      warning,
+      "|",
+      skip,
+      "|",
+      success,
+      "|",
+      collapse = "\n"
+    )
+  )
+
+  test_details <- ""
+
+  vars <- list(
     package = pkgname,
     total_percent = total_percent,
     total_lines_covered = sum(coverage$lines_covered),
@@ -77,8 +131,12 @@ markdown <- function(path = ".", coverage = NULL, output = NULL) {
     total_funcs_hit = sum(coverage$functions_hit),
     total_funcs = sum(coverage$function_count),
     data = data,
+    test_files = test_files,
+    test_details = test_details,
     total_emoji = total_emoji,
-    total_funcs_emoji = total_funcs_emoji
+    total_funcs_emoji = total_funcs_emoji,
+    emoji = list(ok = "✅", fail = "❌", warn = "⚠️", skip = "🦘"),
+    n = totals
   )
 
   lns <- glue::glue_data(
@@ -104,4 +162,105 @@ format_funcs <- function(hit, count) {
 
 format_emoji <- function(x) {
   ifelse(x == 100, "⭐", ifelse(x >= 95, "✅", ifelse(x >= 75, "🛠️", "❌")))
+}
+
+markdown_test_summary <- function(results) {
+  results <- lapply(results, gha_summarize_test)
+  totals <- list(
+    n_fail = sum(vapply(results, "[[", integer(1), "n_fail")),
+    n_warn = sum(vapply(results, "[[", integer(1), "n_warn")),
+    n_skip = sum(vapply(results, "[[", integer(1), "n_skip")),
+    n_ok = sum(vapply(results, "[[", integer(1), "n_ok")),
+    real = sum(vapply(results, "[[", double(1), "real"))
+  )
+
+  # summary -----------------------------------------------------------------
+  gha_summary_write("### Test summary")
+  gha_summary_write()
+  gha_summary_write("| FAIL | WARN | SKIP | PASS | Time |")
+  gha_summary_write("|-----:|-----:|-----:|-----:|:-----|")
+  gha_summary_write(
+    c("|", if (totals$n_fail > 0) totals$n_fail),
+    c("|", if (totals$n_warn > 0) totals$n_warn),
+    c("|", if (totals$n_skip > 0) totals$n_skip),
+    c("|", totals$n_ok),
+    c("|", num_exact(totals$real, 2), "|")
+  )
+
+  # issue details -----------------------------------------------------------
+  gha_summary_write()
+  gha_summary_write("<details>")
+  gha_summary_write("<summary>Test details</summary>")
+
+  gha_summary_write("")
+  gha_summary_write()
+  gha_summary_write("| File | Test | FAIL | WARN | SKIP | PASS | Time |")
+  gha_summary_write("|:-----|:-----|-----:|-----:|-----:|-----:|:-----|")
+
+  issues <- Filter(function(x) length(x$results) != x$n_ok, results)
+  for (issue in issues) {
+    gha_summary_write(
+      c("|", issue$file),
+      c("|", md_escape(issue$test)),
+      c("|", if (totals$n_fail > 0) issue$n_fail),
+      c("|", if (totals$n_warn > 0) issue$n_warn),
+      c("|", if (totals$n_skip > 0) issue$n_skip),
+      c("|", issue$n_ok),
+      c("|", num_exact(issue$real, 2), "|")
+    )
+  }
+  gha_summary_write()
+  gha_summary_write("</details>")
+  gha_summary_write()
+
+  invisible(results)
+}
+
+# Helpers ----------------------------------------------------------------------
+
+gha_summarize_test <- function(test) {
+  test$n_fail <- test$n_skip <- test$n_warn <- test$n_ok <- 0L
+  for (exp in test$results) {
+    if (expectation_broken(exp)) {
+      test$n_fail <- test$n_fail + 1L
+    } else if (expectation_skip(exp)) {
+      test$n_skip <- test$n_skip + 1L
+    } else if (expectation_warning(exp)) {
+      test$n_warn <- test$n_warn + 1L
+    } else {
+      test$n_ok <- test$n_ok + 1L
+    }
+  }
+
+  test
+}
+
+gha_path <- function() {
+  nope <- c("false", "no", "off", "n", "0", "nope", "nay")
+  if (tolower(Sys.getenv("TESTTHAT_GHA_SUMMARY")) %in% nope) {
+    return()
+  }
+
+  if ((out <- Sys.getenv("GITHUB_STEP_SUMMARY")) == "") {
+    return()
+  }
+  out
+}
+
+
+gha_summary_write <- function(...) {
+  path <- gha_path()
+  if (is.null(path)) {
+    return()
+  }
+
+  string <- paste0(c(..., "\n"), collapse = "")
+  Encoding(string) <- "unknown"
+  cat(string, file = path, append = TRUE)
+}
+
+md_escape <- function(x) {
+  x <- gsub("|", "\\|", x, fixed = TRUE)
+  x <- gsub("\n", " ", x, fixed = TRUE)
+  x
 }
--- inst/report.md
+++ inst/report.md
@@ -1,11 +1,29 @@
 ## 📊 {{ package }} test coverage
 
-{{ total_emoji }} **{{ total_percent }}** lines `{{ total_lines_covered }}/{{ total_lines }}` &emsp; {{ total_funcs_emoji }} **{{ total_funcs_percent}}** functions `{{total_funcs_hit }}/{{ total_funcs }}`
+{{ total_emoji }} **{{ total_percent }}** lines \
+`{{ total_lines_covered }}/{{ total_lines }}` &emsp; \
+{{ total_funcs_emoji }} **{{ total_funcs_percent}}** functions \
+`{{total_funcs_hit }}/{{ total_funcs }}`
 
 | File | Lines | Functions | Coverage |
 |:-----|------:|----------:|---------:|
 {{ data }}
 
-## 🧪 {{ package }} test results
+## 📊 {{ package }} test results
 
-TODO
+{{ emoji$fail }} **{{ n$fail }}** failed &nbsp; \
+{{ emoji$warn }} **{{ n$warn }}** warnings &nbsp; \
+{{ emoji$skip }} **{{ n$skip }}** skips \
+{{ emoji$ok }} **{{ n$ok }}** passed &nbsp;
+
+| File | {{ emoji$fail }} Fail | {{ emoji$warn }} Warn | {{ emoji$skip }} Skip | {{ emoji$ok }} Pass |
+|:-----|-----:|-----:|-----:|-----:|
+{{ test_files }}
+
+## 🧪 Test details
+
+<details><summary>See here.</summary>
+| File | Test | Fail | Warn | Skip | Pass |
+|:-----|:-----|-----:|-----:|-----:|-----:|
+{{ test_details }}
+</details>
